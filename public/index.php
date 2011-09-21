03f4a88e3817b3e3859753cae47c52b334324437
Jhon Adrián Cerón <jadrian.ceron@gmail.com>
2011-09-21 12:38:27 -0500

    Agregados ficheros los cuales permiten el inicio de la aplicación.
    Signed-off-by: Jhon Adrián Cerón <jadrian.ceron@gmail.com>

---
+++ public/index.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * Copyright (c) 2011 Jhon Adrián Cerón <jadrian.ceron@gmail.com>
+ *
+ * This file is part of the Plibu project.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+define('DS', DIRECTORY_SEPARATOR);
+define('ROOT', dirname(dirname(__FILE__)));
+
+if(isset($_GET['url']))
+	$url = $_GET['url'];
+
+require_once (ROOT . DS . 'library' . DS . 'bootstrap.php');
