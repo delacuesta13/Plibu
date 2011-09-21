9134a74072f7b414321c3a2e6f1b293756458d95
Jhon Adrián Cerón <jadrian.ceron@gmail.com>
2011-09-21 13:05:08 -0500

    Agregada clase que gestiona la cache del proyecto.
    Signed-off-by: Jhon Adrián Cerón <jadrian.ceron@gmail.com>

---
+++ library/cache.class.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * Copyright (c) 2011 Jhon Adrián Cerón <jadrian.ceron@gmail.com>
+ *
+ * This file is part of the Plibu project.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+class Cache {
+
+	function get($fileName) {
+		$fileName = ROOT.DS.'tmp'.DS.'cache'.DS.$fileName;
+		if (file_exists($fileName)) {
+			$handle = fopen($fileName, 'rb');
+			$variable = fread($handle, filesize($fileName));
+			fclose($handle);
+			return unserialize($variable);
+		} else {
+			return null;
+		}
+	}
+
+	function set($fileName,$variable) {
+		$fileName = ROOT.DS.'tmp'.DS.'cache'.DS.$fileName;
+		$handle = fopen($fileName, 'a');
+		fwrite($handle, serialize($variable));
+		fclose($handle);
+	}
+
+}
