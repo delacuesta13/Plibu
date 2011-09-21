61ac95d4aa40a131bd4a905d10fee16f8ba39ce3
Jhon Adrián Cerón <jadrian.ceron@gmail.com>
2011-09-21 13:42:15 -0500

    Editado copyright del fichero.
    Signed-off-by: Jhon Adrián Cerón <jadrian.ceron@gmail.com>

--- library/vanillacontroller.class.php
+++ library/vanillacontroller.class.php
@@ -3,7 +3,7 @@
 /*
  * Copyright (c) 2011 Jhon Adrián Cerón <jadrian.ceron@gmail.com>
  *
- * This file is part of the Sigabu project.
+ * This file is part of the Plibu project.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
