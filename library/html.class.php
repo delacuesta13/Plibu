7d69aa6a97c2a98437a74501a6373ef9622d503d
Jhon Adrián Cerón <jadrian.ceron@gmail.com>
2011-09-21 13:26:01 -0500

    Agregada clase que genera código HTML.
    Signed-off-by: Jhon Adrián Cerón <jadrian.ceron@gmail.com>

---
+++ library/html.class.php
@@ -0,0 +1,59 @@
+<?php
+
+/*
+ * Copyright (c) 2011 Jhon Adrián Cerón <jadrian.ceron@gmail.com>
+ *
+ * This file is part of the Plibu project.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+class HTML {
+	
+	private $js = array();
+	
+	function link($text,$path) {		
+		$data = '<a href="'.BASE_PATH.'/'.$path.'">'.$text.'</a>';		
+		return $data;
+	}
+	
+	function link_to_function($text,$ejec) {		
+		$data = '<a href="#" onclick="'.$ejec.'; return none;">'.$text.'</a>';		
+		return $data;
+	}
+
+	function includeJs($fileName) {
+		$data = '<script type="text/javascript" src="'.BASE_PATH.'/js/'.$fileName.'.js"></script>';
+		return $data;
+	}
+
+	function includeCss($fileName) {
+		$data = '<link rel="stylesheet" media="screen,projection" type="text/css" href="'.BASE_PATH.'/css/'.$fileName.'.css"/>';
+		return $data;
+	}
+	
+	function includeImg($fileName, $alt){
+		$data = '<img src="'.BASE_PATH.'/img/'.$fileName.'" '.((strlen($alt)>0) ? 'alt="'.$alt.'"' : 'alt="'.$fileName.'"').'/>';
+		return $data;
+	}
+	
+	function javascript_tag($code){
+		$data = '<script type="text/JavaScript">
+        //<![CDATA['.	
+		$code
+		.'
+        //]]>
+        </script>';
+		return $data;		
+	}
+	
+	function css_tag($code){
+		$data = '<style type="text/css">'.	
+		$code
+		.'
+        </style>';
+		return $data;	
+	}
+	
+}
