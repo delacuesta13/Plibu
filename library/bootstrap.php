65258ef74303087ab50c65dcf40b5b047746ab22
Jhon Adrián Cerón <jadrian.ceron@gmail.com>
2011-09-21 13:00:49 -0500

    Agregado fichero de arranque del proyecto.
    Signed-off-by: Jhon Adrián Cerón <jadrian.ceron@gmail.com>

---
+++ library/bootstrap.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * Copyright (c) 2011 Jhon Adrián Cerón <jadrian.ceron@gmail.com>
+ *
+ * This file is part of the Plibu project.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+require_once (ROOT . DS . 'config' . DS . 'config.php');
+require_once (ROOT . DS . 'config' . DS . 'routing.php');
+require_once (ROOT . DS . 'config' . DS . 'inflection.php');
+require_once (ROOT . DS . 'library' . DS . 'shared.php');
