63f1025856d67b5f32aee4e6b378f110c1655f88
Jhon Adrián Cerón <jadrian.ceron@gmail.com>
2011-09-21 13:39:36 -0500

    Agrega clase que genera template (s).
    Signed-off-by: Jhon Adrián Cerón <jadrian.ceron@gmail.com>

---
+++ library/template.class.php
@@ -0,0 +1,59 @@
+<?php
+
+/*
+ * Copyright (c) 2011 Jhon Adrián Cerón <jadrian.ceron@gmail.com>
+ *
+ * This file is part of the Plibu project.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+class Template {
+	
+	protected $variables = array();
+	protected $_controller;
+	protected $_action;
+	
+	function __construct($controller,$action) {
+		$this->_controller = $controller;
+		$this->_action = $action;
+	}
+	
+	/** Set Variables **/
+	
+	function set($name,$value) {
+		$this->variables[$name] = $value;
+	}
+	
+	/** Display Template **/
+	
+	function render($doNotRenderHeader = 0) {
+		
+		$html = new HTML;
+		extract($this->variables);
+		
+		if ($doNotRenderHeader == 0) {
+				
+			if (file_exists(ROOT . DS . 'application' . DS . 'views' . DS . $this->_controller . DS . 'header.php')) {
+				include (ROOT . DS . 'application' . DS . 'views' . DS . $this->_controller . DS . 'header.php');
+			} else {
+				include (ROOT . DS . 'application' . DS . 'views' . DS . 'header.php');
+			}
+		}
+		
+		if (file_exists(ROOT . DS . 'application' . DS . 'views' . DS . $this->_controller . DS . $this->_action . '.php')) {
+			include (ROOT . DS . 'application' . DS . 'views' . DS . $this->_controller . DS . $this->_action . '.php');
+		}
+		
+		if ($doNotRenderHeader == 0) {
+				
+			if (file_exists(ROOT . DS . 'application' . DS . 'views' . DS . $this->_controller . DS . 'footer.php')) {
+				include (ROOT . DS . 'application' . DS . 'views' . DS . $this->_controller . DS . 'footer.php');
+			} else {
+				include (ROOT . DS . 'application' . DS . 'views' . DS . 'footer.php');
+			}
+		}		
+	}
+	
+}
