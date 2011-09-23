ffa139e1a955ee2c13fdd406b4bffada9496e7c2
Jhon Adrián Cerón <jadrian.ceron@gmail.com>
2011-09-23 17:59:28 -0500

    Eliminados ficheros que no se utilizarán.

---
+++ application/views/actividades/index.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * Copyright (c) 2011 Jhon Adrián Cerón <jadrian.ceron@gmail.com>
+ *
+ * This file is part of the Plibu project.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+?>
+
+<div class="page-header">
+	<h1>
+		Actividades
+		<?php echo (isset($periodo_actual)) ? ('<small>' . $periodo_actual[0]['Periodo']['periodo'] . '</small>') : ''?>
+	</h1>
+</div>
+
+<div class="row">
+	<div class="span12 offset2">
+	</div>
+</div><!-- /row -->
