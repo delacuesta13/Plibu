aeaa83795a4ecdd9402299af54008faa60385180
Jhon Adrián Cerón <jadrian.ceron@gmail.com>
2011-09-23 17:04:36 -0500

    Implementado píe de página de la aplicación.
    Signed-off-by: Jhon Adrián Cerón <jadrian.ceron@gmail.com>

---
+++ application/views/footer.php
@@ -0,0 +1,31 @@
+<?php
+/*
+ * Copyright (c) 2011 Jhon Adrián Cerón <jadrian.ceron@gmail.com>
+ *
+ * This file is part of the Plibu project.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+?>
+
+			</section><!-- /content -->
+	  		
+			<footer>
+				<p class="pull-right">Developed by Jhon Adrián Cerón Guzmán<br/>
+					<a href="http://www.w3.org/html/logo/" style="float:right" target="_blank">
+					<img src="http://www.w3.org/html/logo/badge/html5-badge-h-css3-semantics.png" width="103" height="40"
+					alt="HTML5 Powered with CSS3 / Styling, and Semantics" title="HTML5 Powered with CSS3 / Styling, and Semantics">
+					</a>
+				</p>
+				<p>
+					Copyright &copy; 2011
+					<a href="http://ucc.edu.co/" target="_blank">Universidad Cooperativa de Colombia</a> -
+					Sede <a href="http://ucc.edu.co/cali/Paginas/UniversidadCooperativadeColombia_Cali.aspx" target="_blank">Cali</a>
+				</p>
+			</footer>
+      
+		</div><!-- /container -->
+		
+	</body>
+</html>
