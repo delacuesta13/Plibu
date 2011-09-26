725d3291a4257239d31af249adb89dfefac0be84
Jhon Adrián Cerón <jadrian.ceron@gmail.com>
2011-09-26 18:30:07 -0500

    Implementado formulario (para interactuar con la tabla de cursos) y div donde se carga los cursos, vía ajax.
    Signed-off-by: Jhon Adrián Cerón <jadrian.ceron@gmail.com>

--- application/views/actividades/index.php
+++ application/views/actividades/index.php
@@ -12,11 +12,63 @@
 <div class="page-header">
 	<h1>
 		Actividades
-		<?php echo (isset($periodo_actual)) ? ('<small>' . $periodo_actual[0]['Periodo']['periodo'] . '</small>') : ''?>
+		<?php echo (isset($periodo_actual)) ? ('<small>Periodo ' . $periodo_actual[0]['Periodo']['periodo'] . '</small>') : ''?>
 	</h1>
 </div>
 
 <div class="row">
-	<div class="span12 offset2">
-	</div>
+	<div class="span16">
+		
+		<div class="row">
+			<div class="span16">
+				<form>
+					<fieldset>
+						<div class="row">
+							<div class="span8">
+								<div class="clearfix">
+									<label for="regpag">Mostrar</label>
+									<div class="input">
+										<select class="medium" name="regpag" id="regpag">
+											<option value="<?php echo PAGINATE_LIMIT?>"><?php echo PAGINATE_LIMIT?></option>
+											<option value="20">20</option>
+											<option value="50">50</option>
+											<option value="100">100</option>
+										</select>
+										<span class="help-inline">registros por página</span>
+									</div><!-- /input -->
+								</div><!-- /clearfix -->
+							</div><!-- /span8 -->
+							<div class="span8">
+								<div class="row">
+									<div class="span6">
+										<div class="clearfix">
+											<input class="span6" id="search" name="search" size="30" type="search"/>
+										</div><!-- /clearfix -->
+									</div><!-- /span6 -->
+									<div class="span2">
+										<a href="javascript:void(0);" class="btn primary" id="btn_search">Buscar</a>
+									</div><!-- /span2 -->
+								</div><!-- /row -->
+							</div><!-- /span8 -->
+						</div><!-- /row -->
+					</fieldset>
+				</form>
+			</div><!-- /span16 -->
+		</div><!-- /row -->
+		
+		<div class="row">
+			<div id="modal-cargando" class="modal hide fade">
+				<div class="modal-header">
+					<h3>Cargando...</h3>
+				</div>
+				<div class="modal-body">
+					<p>Espere por favor, esta operación puede tardar algún tiempo.</p>
+				</div>
+			</div>
+			<!-- div donde cargo el ajax -->
+			<div class="span16" id="dynamic">
+			</div><!-- /span16 -->
+		</div><!-- /row -->
+		
+	</div><!-- /span16 -->
 </div><!-- /row -->
