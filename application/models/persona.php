6714802b9810251b1a180feb54b7ec3b0873a93a
Jhon Adrián Cerón <jadrian.ceron@gmail.com>
2011-09-27 18:00:14 -0500

    Agregada función que devuelve el perfil de una persona en un determinado período.
    Signed-off-by: Jhon Adrián Cerón <jadrian.ceron@gmail.com>

--- application/models/persona.php
+++ application/models/persona.php
@@ -15,4 +15,25 @@ class Persona extends VanillaModel {
 		return $this->query('select * from personas where dni = \'' . mysql_real_escape_string($dni) . '\'');
 	}
 	
+	function consultar_perfil ($dni, $idPeriodo) {
+		$sql = '
+		SELECT	perfil.id,
+				periodo.periodo,
+				multientidad.nombre, 
+				persona.tipo_dni, 
+				persona.dni, 
+				persona.nombres, 
+				persona.apellidos
+		FROM	perfiles perfil,
+				periodos periodo, 
+				multientidad multientidad, 
+				personas persona 
+		WHERE	perfil.periodo_id = \'' . mysql_real_escape_string($idPeriodo) . '\'
+				AND perfil.periodo_id = periodo.id 
+				AND perfil.persona_dni = \'' . mysql_real_escape_string($dni) . '\' 
+				AND perfil.persona_dni = persona.dni 
+				AND perfil.perfil_multientidad = multientidad.id 
+		';
+	}
+	
 }
